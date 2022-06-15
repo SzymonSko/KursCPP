4b0e3e9d5985c443fb73d9d50e6cd11f6645792e
SzymonSko <76167839+SzymonSko@users.noreply.github.com>
2022-06-15 12:45:55 +0200

    Zaliczenie

---
+++ kcppZadania/ZadClassKcpp/include/ZadCinCoutFile.h
@@ -0,0 +1,12 @@
+#ifndef CINCOUT_H
+#define CINCOUT_H 1
+#include <iostream>
+#include <fstream>
+class cincout{
+    public:
+        cincout(){this->main();}
+    private:
+        int main();
+};
+
+#endif
---
+++ kcppZadania/ZadClassKcpp/include/ZadPrzekazywanieTablicKopia.h
@@ -0,0 +1,11 @@
+#ifndef PRZEKAZYWANIE_H
+#define PRZEKAZYWANIE_H 1
+#include <iostream>
+class przekazywanie{
+    public:
+        przekazywanie(){this->main();}
+    private:
+        void func(int tab[]);
+        int main();
+};
+#endif
